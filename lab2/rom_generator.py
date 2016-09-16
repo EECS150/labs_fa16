c28dedb8d365f83ecc794b7b5158766214aeff03
Class Account <eecs151-tab@c125m-15.EECS.Berkeley.EDU>
2016-09-16 12:11:06 -0700

    added rom generating materials

--- lab2/rom_generator.py
+++ lab2/rom_generator.py
@@ -2,29 +2,33 @@ import wave
 import random
 import struct
 import sys
+import math
 
-output_wav = wave.open('output.wav', 'w')
-output_wav.setparams((2, 2, 44100, 0, 'NONE', 'not compressed'))
+generated_verilog_filepath = sys.argv[1]
+memory_file = sys.argv[2]
+memory_depth = int(sys.argv[3])
+memory_width = int(sys.argv[4])
+address_bits = int(math.ceil(math.log(memory_depth, 2)))
+data_bits = memory_width
 
-values = []
+with open(memory_file, 'r') as memory_data:
+    data = memory_data.read()
 
-filepath = sys.argv[1]
-with open(filepath, 'r') as samples_file:
-    data = samples_file.read()
+data = data.split('\n')
 
-for note in data:
-    value = 0
-    if (note == '0'):
-        value = -20000
-    elif (note == '1'):
-        value = 20000
+verilog_file = open(generated_verilog_filepath, 'w')
+verilog_file.write("module rom (input [%d:0] address, output reg [%d:0] data);\n" % (address_bits - 1, data_bits - 1))
+verilog_file.write("\talways @ (*) begin\n")
+verilog_file.write("\t\tcase(address)\n")
+
+for i in range(0, memory_depth):
+    if (i >= len(data) or len(data[i]) == 0): # Write a 0
+        verilog_file.write("\t\t\t%d'd%d: %d'd%d\n" % (address_bits, i, data_bits, 0))
     else:
-        continue
-    packed_value = struct.pack('h', value)
-    values.append(packed_value)
-    values.append(packed_value)
+        verilog_file.write("\t\t\t%d'd%d: %d'd%d\n" % (address_bits, i, data_bits, int(data[i])))
 
-value_str = ''.join(values)
-output_wav.writeframes(value_str)
-output_wav.close()
-sys.exit(0)
+verilog_file.write("\t\tendcase\n")
+verilog_file.write("\tend\n")
+verilog_file.write("endmodule")
+verilog_file.close()
+
